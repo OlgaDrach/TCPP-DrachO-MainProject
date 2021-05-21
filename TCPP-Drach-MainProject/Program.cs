993fcb9ec9eec893af078b5db746313d57a4e848
Ольга Драч <drach.o@donnu.edu.ua>
2021-05-21 11:11:21 +0300

    Добавьте файлы проекта.

---
+++ TCPP-Drach-MainProject/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace TCPP_Drach_MainProject
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
