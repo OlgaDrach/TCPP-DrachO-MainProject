de7f305db1964ed2991cdf5e24ac77052e033d0f
OlgaDrach <61631856+OlgaDrach@users.noreply.github.com>
2021-05-21 11:31:21 +0300

    Update Program.cs

--- TCPP-Drach-MainProject/Program.cs
+++ TCPP-Drach-MainProject/Program.cs
@@ -10,6 +10,8 @@ namespace TCPP_Drach_MainProject
             Console.WriteLine("Hello World!");
             Console.WriteLine("Hello World!");
             Console.WriteLine("Hello World!");
+            Console.WriteLine("Hello World!");
+           
         }
     }
 }
